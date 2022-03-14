516db290b602073022d2be7c89f011dd7d89e67d
Tyanaeffa12 <tyanaeffa@yahoo.com>
2022-03-14 15:41:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main - entry point
  *
- * Return: o after printing the function
+ * Return: 0 after printing the function
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return
+	return (0);
 }
 
